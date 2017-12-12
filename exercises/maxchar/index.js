0fe5329011acd8d41a10b5ccf5afc36bb0b83d9b
Vitaly-V <gtopbox@gmail.com>
2017-12-12 19:46:53 +0200

    Max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,10 +1,21 @@
-// --- Directions
+    // --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const strObj = {};
+  for(const v of str) {
+    strObj[v] =  strObj[v] + 1 || 1;
+  }
+  return Object.keys(strObj).reduce((prev, cur) => {
+    if (strObj[prev] > strObj[cur]) {
+      return prev;
+    }
+    return cur;
+  });
+}
 
 module.exports = maxChar;
