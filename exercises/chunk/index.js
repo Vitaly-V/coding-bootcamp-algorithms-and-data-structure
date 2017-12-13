707e76b073e571f1898540fd56fd9f3c64ed7f67
Vitaly-V <gtopbox@gmail.com>
2017-12-13 08:56:33 +0200

    Chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,44 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+/*
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
+
+ function chunk(array, size) {
+   const chunked = [];
+
+   for (let element of array) {
+     const last = chunked[chunked.length - 1];
+
+     if (!last || last.length === size) {
+       chunked.push([element]);
+     } else {
+       last.push(element);
+     }
+   }
+
+   return chunked;
+ }
+ */
+
+function chunk(array, size) {
+  chunksQty = Math.ceil(array.length / size);
+  chunkedArr = [];
+  for (let i = 0; i < chunksQty; i++) {
+    chunkedArr.push(array.slice(i * size, i * size + size));
+  }
+  return chunkedArr;
+}
+
 
 module.exports = chunk;
