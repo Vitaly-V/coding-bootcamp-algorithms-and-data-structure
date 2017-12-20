5e6e31e9c4717fbfd2092567734daa7179a786fe
Vitaly-V <gtopbox@gmail.com>
2017-12-20 21:28:57 +0200

    Fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,65 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+/*
+function fib(n) {
+  const arr = []
+  let prev = 0, next = 1;
+
+  for (let i = 0; i <= n; i++) {
+    arr.push(prev);
+    let sum = prev + next;
+    prev = next;
+    next = sum;
+  }
+  return arr[arr.length - 1];
+}
+*/
+
+/*
+function fib(n) {
+  arr = fibGeneraror(n);
+  return arr[n];
+}
+
+function fibGeneraror(n, arr=[0,1]) {
+  if (arr.length <= n) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    fibGeneraror(n, arr);
+  }
+  return arr;
+}
+*/
+
+/*
+function fib(n) {
+  const a = Math.pow(((1 + Math.sqrt(5)) / 2), n);
+  const b = Math.pow(((1 - Math.sqrt(5)) / 2), n);
+  return (1 / Math.sqrt(5)) * (a - b)
+}
+*/
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n -1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
