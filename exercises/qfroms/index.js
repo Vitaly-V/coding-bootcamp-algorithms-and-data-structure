40ca15fa1957f38b6499c8f1a105492eeb057b8c
Vitaly-V <gtopbox@gmail.com>
2017-12-24 14:35:17 +0200

    Implement a Queue datastructure using two stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,78 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+/*
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+}
+*/
+
+class Queue {
+  constructor() {
+    this.s1 = new Stack();
+    this.s2 = new Stack();
+  }
+
+  add(item) {
+    this.s1.push(item);
+  }
+
+  remove() {
+    return this.process('pop');
+  }
+
+  peek() {
+    return this.process('peek');
+  }
+
+  process(what = 'remove') {
+    [this.s1, this.s2] = this.convertStacks(this.s1, this.s2);
+    const item = this.s2[what]();
+    [this.s2, this.s1] = this.convertStacks(this.s2, this.s1);
+    return item;
+  }
+
+  convertStacks(from, to) {
+    while (from.peek() !== undefined) {
+      to.push(from.pop());
+    }
+    return [from, to];
+  }
+}
 
 module.exports = Queue;
