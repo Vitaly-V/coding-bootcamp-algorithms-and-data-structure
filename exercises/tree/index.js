a526c8859c581b1beb399e0b4bc22c5944e01af9
Vitaly-V <gtopbox@gmail.com>
2017-12-26 22:49:30 +0200

    Tree iterating

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,43 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while(arr.length) {
+      arr.push(...arr[0].children);
+      fn(arr[0]);
+      arr.shift();
+    }
+  }
+
+  traverseDF(fn, arr = [this.root]) {
+    arr.forEach(node => {
+    fn(node);
+    if (node.children.length) {
+      this.traverseDF(fn, node.children);
+    }
+    });
+  }
+}
 
 module.exports = { Tree, Node };
