8772da72b9eb7c4fa34170d7fd8f7997c5fd3b4f
Vitaly-V <gtopbox@gmail.com>
2017-12-27 10:11:07 +0200

    tree level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const d = 'delimiter';
+  const nodes = [root, d];
+  const arr = [1];
+  while (nodes.length > 1) {
+    const node = nodes.shift();
+    if (node === d) {
+      arr.push(nodes.length);
+      nodes.push(d);
+    } else {
+      nodes.push(...node.children);
+    }
+  }
+  return arr;
+}
 
 module.exports = levelWidth;
